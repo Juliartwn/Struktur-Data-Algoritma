b85db0ee6e7462ee69861aa21800d89defd3d2f4
Juliartwn <127955383+Juliartwn@users.noreply.github.com>
2023-05-02 15:16:38 +0800

    Add files via upload

---
+++ LinkedList/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include "LinkedList.h" 
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	LinkedList list1;
+	
+	list1.insertToHead (10); 
+	list1.insertToHead (20);
+	list1.insertToHead (30); 
+	list1.insertToTail (50);
+	list1.insertAfter(20,55);
+
+	cout << "head: " << list1.head->value << endl;
+	cout<<"tail: " << list1.tail->value << endl; 
+	cout << "Nilai node 3: " << list1.head->next->next->value << endl;
+	
+//	cout << "Urutan Linkedlist 1: ";
+	list1.printAll();
+	
+	LinkedList list2;
+	list2.insertToHead (24); 
+	list2.insertToHead (30);
+
+	cout << "Urutan Linkedlist 2: ";
+	list2.printAll();
+	
+	return 0;
+}
